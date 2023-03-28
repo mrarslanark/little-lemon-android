02af8ce4c5323271ae7640e3c44c9aa0fe31b7ea
Arslan Mushtaq <arslanark@gmail.com>
2023-03-29 02:39:18 +0500

    Add navigation and update profile screen

--- app/src/main/java/com/arslan/littlelemon/Profile.kt
+++ app/src/main/java/com/arslan/littlelemon/Profile.kt
@@ -1,9 +1,144 @@
 package com.arslan.littlelemon
 
-import androidx.compose.material.Text
+import android.content.SharedPreferences
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import com.arslan.littlelemon.components.BrandButton
+import com.arslan.littlelemon.navigation.Onboarding
+import com.arslan.littlelemon.ui.theme.BrandColors
+import com.arslan.littlelemon.ui.theme.BrandTypography
 
 @Composable
-fun ProfileScreen() {
-    Text(text = "Welcome to profile Screen")
+fun ProfileScreen(navController: NavHostController, sharedPreferences: SharedPreferences) {
+
+    val firstName = sharedPreferences.getString(MainActivity.FIRST_NAME_KEY, "")
+    val lastName = sharedPreferences.getString(MainActivity.LAST_NAME_KEY, "")
+    val location = sharedPreferences.getString(MainActivity.LOCATION_KEY, "")
+    val emailAddress = sharedPreferences.getString(MainActivity.EMAIL_ADDRESS_KEY, "")
+
+    val orderStatues = remember { mutableStateOf(false) }
+    val passwordChanges = remember { mutableStateOf(false) }
+    val specialOffers = remember { mutableStateOf(false) }
+    val newsletter = remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier.padding(16.dp),
+    ) {
+        Column(modifier = Modifier.weight(0.9f, true)) {
+            Text(
+                text = "Personal Information",
+                style = BrandTypography.Subtitle
+            )
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 16.dp),
+                verticalAlignment = Alignment.Top
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.placeholder_image),
+                    contentDescription = "Person",
+                    modifier = Modifier
+                        .padding(end = 12.dp)
+                        .size(75.dp)
+                        .clip(CircleShape),
+                    contentScale = ContentScale.FillHeight,
+                )
+                Column {
+                    Text(text = "$firstName $lastName".uppercase(), style = BrandTypography.SectionTitle)
+                    Text(text = "$emailAddress", style = BrandTypography.ParagraphText)
+                    Text(text = "$location", style = BrandTypography.ParagraphText)
+                }
+            }
+
+            Column(modifier = Modifier.padding(vertical = 12.dp)) {
+                Text(
+                    text = "Email Notifications",
+                    style = BrandTypography.LeadText,
+                    modifier = Modifier.padding(bottom = 8.dp)
+                )
+                BrandCheckbox("Order Statuses", orderStatues.value) {
+                    orderStatues.value = it
+                }
+                BrandCheckbox("Password Changes", passwordChanges.value) {
+                    passwordChanges.value = it
+                }
+                BrandCheckbox("Special Offers", specialOffers.value) {
+                    specialOffers.value = it
+                }
+                BrandCheckbox("Newsletter", newsletter.value) {
+                    newsletter.value = it
+                }
+            }
+        }
+
+        Row(
+            modifier = Modifier.weight(0.1f)
+        ) {
+            BrandButton(
+                modifier = Modifier
+                    .weight(0.5f, true)
+                    .padding(end = 2.dp),
+                text = "Logout",
+                backgroundColor = BrandColors.Primary,
+                textColor = Color.White
+            ) {
+                sharedPreferences.edit().clear().apply()
+                navController.navigate(Onboarding.route) {
+                    popUpTo(Onboarding.route) {
+                        inclusive = true
+                    }
+                }
+            }
+
+            BrandButton(
+                modifier = Modifier
+                    .weight(0.5f, true)
+                    .padding(start = 2.dp),
+                text = "Edit",
+            ) {
+                // TODO: Navigate to Edit Profile Screen
+            }
+        }
+
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun BrandCheckbox(label: String, status: Boolean, onCheckedChange: (Boolean) -> Unit) {
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        CompositionLocalProvider(LocalMinimumTouchTargetEnforcement provides false) {
+            Checkbox(
+                checked = status,
+                onCheckedChange = onCheckedChange,
+                colors = CheckboxDefaults.colors(
+                    checkedColor = BrandColors.PrimaryVariant
+                ),
+                modifier = Modifier.padding(end = 8.dp)
+            )
+        }
+        Text(text = label, style = BrandTypography.ParagraphText)
+    }
 }
