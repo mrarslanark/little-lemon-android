09009fa8f60ccefca7195af05a083069eda62989
Arslan Mushtaq <arslanark@gmail.com>
2023-03-29 15:01:20 +0500

    Fetch data from API and store in room database

--- app/src/main/java/com/arslan/littlelemon/MainActivity.kt
+++ app/src/main/java/com/arslan/littlelemon/MainActivity.kt
@@ -1,16 +1,42 @@
 package com.arslan.littlelemon
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.RemoteViews.RemoteCollectionItems
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.lifecycleScope
+import androidx.room.Room
 import com.arslan.littlelemon.navigation.NavigationComposable
 import com.arslan.littlelemon.ui.theme.LittleLemonTheme
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.engine.android.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+    private val httpClient = HttpClient(Android) {
+        install(ContentNegotiation) {
+            json(contentType = ContentType("text", "plain"))
+        }
+    }
+
+    private val database by lazy {
+        Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java,
+            "database"
+        ).build()
+    }
 
     private val sharedPreferences by lazy {
         getSharedPreferences("LittleLemon", MODE_PRIVATE)
@@ -29,6 +55,25 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+        lifecycleScope.launch {
+            if (database.menuItemDao().isEmpty()) {
+                val menuItems = fetchMenu()
+                saveMenuToDatabase(menuItems)
+            }
+        }
+    }
+
+    private suspend fun fetchMenu(): List<MenuItem> {
+        val url = "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu.json"
+        val response: MenuData = httpClient.get(url).body()
+        response.menu.forEach { Log.d("MainActivity", it.title) }
+        return response.menu
+    }
+
+    private fun saveMenuToDatabase(menuItems: List<MenuItem>) {
+        val menuItemsRoom = menuItems.map { it.toMenuItemRoom() }
+        database.menuItemDao().insertAll(*menuItemsRoom.toTypedArray())
     }
 
     companion object {
--- app/src/main/java/com/arslan/littlelemon/Network.kt
+++ app/src/main/java/com/arslan/littlelemon/Network.kt
@@ -4,16 +4,25 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class MenuNetworkData(
-    @SerialName("menu") val menu: List<MenuItemNetwork>
+data class MenuData(
+    @SerialName("menu") val menu: List<MenuItem>
 )
 
 @Serializable
-data class MenuItemNetwork(
+data class MenuItem(
     @SerialName("id") val id: Int,
     @SerialName("title") val title: String,
     @SerialName("description") val description: String,
     @SerialName("price") val price: String,
     @SerialName("image") val image: String,
     @SerialName("category") val category: String
-)
+) {
+    fun toMenuItemRoom() = MenuItemRoom(
+        id = id,
+        title = title,
+        description = description,
+        price = price,
+        image = image,
+        category = category
+    )
+}
